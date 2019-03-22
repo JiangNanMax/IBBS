44c02cf1e66e8608b74e4c3a42d7986a8fac05c3
JiangNanMax <31010026+JiangNanMax@users.noreply.github.com>
2019-03-22 15:45:33 +0800

    故障恢复

--- admin/father_module_add.php
+++ admin/father_module_add.php
@@ -5,14 +5,11 @@
  * Date: 2019/3/20
  * Time: 15:57
  */
-
 include_once '../inc/config.inc.php';
 include_once '../inc/mysql.inc.php';
 include_once '../inc/tool.inc.php';
-
 $template['title'] = '添加父版块';
 $template['css'] = array('css/index.css');
-
 if (isset($_POST['submit'])) {
     $conn = connect();
     $query = "insert into ibbs_father_module (module_name, sort) values ('{$_POST['module_name']}', '{$_POST['sort']}')";
@@ -26,26 +23,26 @@ if (isset($_POST['submit'])) {
 }
 ?>
 <?php include 'inc/header.inc.php' ?>
-<div id="main">
-    <div class="title" style="margin-bottom:20px;">添加父版块</div>
-    <form method="post">
-        <table class="au">
-            <tr>
-                <td>版块名称</td>
-                <td><input name="module_name" type="text" /></td>
-                <td class="note">
-                    版块名称不得为空，且最长不得超过50个字符
-                </td>
-            </tr>
-            <tr>
-                <td>排序优先级</td>
-                <td><input name="sort" type="text" /></td>
-                <td class="note">
-                    填写一个数字即可，越大则优先级越高
-                </td>
-            </tr>
-        </table>
-        <input style="margin-left:110px;margin-top:20px;cursor:pointer;" class="btn" type="submit" name="submit" value="添加" />
-    </form>
-</div>
+    <div id="main">
+        <div class="title" style="margin-bottom:20px;">添加父版块</div>
+        <form method="post">
+            <table class="au">
+                <tr>
+                    <td>版块名称</td>
+                    <td><input name="module_name" type="text" /></td>
+                    <td class="note">
+                        版块名称不得为空，且最长不得超过50个字符
+                    </td>
+                </tr>
+                <tr>
+                    <td>排序优先级</td>
+                    <td><input name="sort" type="text" /></td>
+                    <td class="note">
+                        填写一个数字即可，越大则优先级越高
+                    </td>
+                </tr>
+            </table>
+            <input style="margin-left:110px;margin-top:20px;cursor:pointer;" class="btn" type="submit" name="submit" value="添加" />
+        </form>
+    </div>
 <?php include 'inc/footer.inc.php' ?>
--- admin/father_module_update.php
+++ admin/father_module_update.php
@@ -8,7 +8,6 @@
 include_once '../inc/config.inc.php';
 include_once '../inc/mysql.inc.php';
 include_once '../inc/tool.inc.php';
-
 $template['title'] = '修改父版块';
 $template['css'] = array('css/index.css');
 if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
@@ -35,26 +34,26 @@ if (isset($_POST['submit'])) {
 $data = mysqli_fetch_assoc($result);
 ?>
 <?php include './inc/header.inc.php'?>
-<div id="main">
-    <div class="title" style="margin-bottom:20px;">修改父版块 - <?php echo $data['module_name']?></div>
-    <form method="post">
-        <table class="au">
-            <tr>
-                <td>版块名称</td>
-                <td><input name="module_name" value="<?php echo $data['module_name']?>" type="text" /></td>
-                <td class="note">
-                    版块名称不得为空，且最长不得超过50个字符
-                </td>
-            </tr>
-            <tr>
-                <td>排序优先级</td>
-                <td><input name="sort" value="<?php echo $data['sort']?>" type="text" /></td>
-                <td class="note">
-                    填写一个数字即可，越大则优先级越高
-                </td>
-            </tr>
-        </table>
-        <input style="margin-top:20px;cursor:pointer;" class="btn" type="submit" name="submit" value="修改" />
-    </form>
-</div>
+    <div id="main">
+        <div class="title" style="margin-bottom:20px;">修改父版块 - <?php echo $data['module_name']?></div>
+        <form method="post">
+            <table class="au">
+                <tr>
+                    <td>版块名称</td>
+                    <td><input name="module_name" value="<?php echo $data['module_name']?>" type="text" /></td>
+                    <td class="note">
+                        版块名称不得为空，且最长不得超过50个字符
+                    </td>
+                </tr>
+                <tr>
+                    <td>排序优先级</td>
+                    <td><input name="sort" value="<?php echo $data['sort']?>" type="text" /></td>
+                    <td class="note">
+                        填写一个数字即可，越大则优先级越高
+                    </td>
+                </tr>
+            </table>
+            <input style="margin-top:20px;cursor:pointer;" class="btn" type="submit" name="submit" value="修改" />
+        </form>
+    </div>
 <?php include './inc/footer.inc.php'?>
