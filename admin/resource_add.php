13b796a66a652c0cbe5147a132ccef8f3648ccea
JiangNanMax <31010026+JiangNanMax@users.noreply.github.com>
2019-05-18 16:01:12 +0800

    debug

--- admin/resource_add.php
+++ admin/resource_add.php
@@ -26,12 +26,34 @@ if (isset($_POST['pic'])) {
         skip('resource_add.php', 'error', $upload['error']);
     }
     else {
-        setcookie('savepath', $save_path, time() + 300);
+        setcookie('picsavepath', $save_path, time() + 3600);
     }
 }
+
+if (isset($_POST['book'])) {
+    $save_path = '../resources/uploads'.date('/Y/m/d/');
+    $upload = upload($save_path,'200M','book');
+    if(!$upload['return']) {
+        $save_path = '';
+        skip('resource_add.php', 'error', $upload['error']);
+    }
+    else {
+        setcookie('booksavepath', $save_path, time() + 3600);
+    }
+}
+
 if (isset($_POST['submit'])) {
-    $pic_path = $_COOKIE['savepath'];
-    skip('resource_add.php', 'ok', $pic_path);
+    //成功拿到
+    $pic_path = $_COOKIE['picsavepath'];
+    $book_path = $_COOKIE['booksavepath'];
+    $query = "insert into ibbs_resource (title, introduction, url, photo, add_time) values ('{$_POST['title']}', '{$_POST['info']}', '{$book_path}', '{$pic_path}', now())";
+    execute($conn, $query);
+    if (mysqli_affected_rows($conn) == 1) {
+        skip('resources.php', 'ok', '动态添加成功！');
+    }
+    else {
+        skip('resource_add.php', 'error', '动态添加失败，请重试！');
+    }
 }
 
 ?>
@@ -47,12 +69,28 @@ if (isset($_POST['submit'])) {
                     <input class="btn" type="submit" name="pic" value="保存" />
                 </td>
                 <td class="note">
-                    务必添加封面图片,添加后请点击保存按钮，页面刷新完成后再继续添加下列内容
+                    务必添加封面图片(最大2M),添加后请点击保存按钮，页面刷新完成后再继续添加下列内容
                 </td>
 
             </tr>
         </table>
     </form>
+
+    <form method="post" enctype="multipart/form-data">
+        <table class="au">
+            <tr>
+                <td>资源文件</td>
+                <td>
+                    <input style="cursor:pointer;" width="100" type="file" name="pdf" /><br /><br />
+                    <input class="btn" type="submit" name="book" value="保存" />
+                </td>
+                <td class="note">
+                    务必添加资源文件(当前仅支持.pdf文件,最大200M),添加后请点击保存按钮，页面刷新完成后再继续添加下列内容
+                </td>
+            </tr>
+        </table>
+    </form>
+
     <form method="post">
         <table class="au">
             <tr>
